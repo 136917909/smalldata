9c876ee09ce78fc9166fd1b6c89771c50bd4888a
TOMMY <1031680773@qq.com>
2020-01-03 21:51:06 +0800

    hot point

---
+++ src/ui/hotpiont.vue
@@ -0,0 +1,23 @@
+<template>
+    <div style="position: relative;width: 200px;">
+      <div style="position: absolute;top:10px;left:10px;">大数据</div>
+      <div style="position: absolute;top:30px;left:100px;">云计算</div>
+      <div style="position: absolute;top:50px;left:30px;">区块链</div>
+      <div style="position: absolute;top:70px;left:10px;">手机app</div>
+      <div style="position: absolute;top:90px;left:100px;">IT</div>
+      <div style="position: absolute;top:110px;left:50px;">金融</div>
+      <div style="position: absolute;top:130px;left:80px;">为人民服务</div>
+      <div style="position: absolute;height: 3px;background: black;top:150px"></div>
+      <div style="position: absolute;bottom: 0px;">推荐职业：</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "hotpiont"
+    }
+</script>
+
+<style scoped>
+
+</style>
